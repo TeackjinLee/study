7813d9ca20e735f88a17ed36d1a37464c886ba1a
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-05-01 16:38:21 +0900

    로그인 페이지 구현 작업

--- study/src/main/java/com/springboot/study/api/config/SecurityConfig.java
+++ study/src/main/java/com/springboot/study/api/config/SecurityConfig.java
@@ -1,14 +1,8 @@
 package com.springboot.study.api.config;
 
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
