87b1840003f1ccfd85a3d8f2c79e3879f6c404b5
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-05-04 23:42:24 +0900

    로그인 페이지 구현 작업 TokenProvider 생성

--- study/src/main/java/com/springboot/study/api/common/config/jwt/TokenProvider.java
+++ study/src/main/java/com/springboot/study/api/common/config/jwt/TokenProvider.java
@@ -1,12 +1,11 @@
 package com.springboot.study.api.common.config.jwt;
 
 import com.springboot.study.datasource.common.TokenDto;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -24,6 +23,8 @@ import java.util.stream.Collectors;
 
 @Component
 public class TokenProvider {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
@@ -80,7 +81,23 @@ public class TokenProvider {
         return new UsernamePasswordAuthenticationToken(principal, "", authorites);
     }
 
-    public Claims parseClaims(String accessToken) {
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+    private Claims parseClaims(String accessToken) {
         try {
             return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
         } catch (ExpiredJwtException e) {
