9a901f1855da67da4b92bee8b1b23064b483b364
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-05-06 21:20:29 +0900

    멋사 SpringSecurity 인증인가 - 02 JWT(JsonWebToken) 발급

--- security/src/main/java/com/springboot/security/controller/UserController.java
+++ security/src/main/java/com/springboot/security/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.springboot.security.controller;
 
+import com.springboot.security.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @PostMapping("/join")
     public ResponseEntity<String> join() {
         return ResponseEntity.ok().body("회원가입이 되었습니다.");
     }
+
     @PostMapping("/login")
     public ResponseEntity<String> login() {
-        return ResponseEntity.ok().body("token");
+        return ResponseEntity.ok().body(userService.login("", ""));
     }
 
 }
---
+++ security/src/main/java/com/springboot/security/service/UserService.java
@@ -0,0 +1,20 @@
+package com.springboot.security.service;
+
+import com.springboot.security.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    // secretKey
+    @Value("{jwt.secret}")
+    private String secretKey;
+
+    private Long expiredMs = 1000 * 60 * 60l;
+
+    public String login(String userName, String password) {
+        // 인증과정 생략
+        return JwtUtil.createJwt(userName, secretKey, expiredMs);
+    }
+}
