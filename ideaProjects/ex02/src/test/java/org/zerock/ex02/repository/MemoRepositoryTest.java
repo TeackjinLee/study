955313d13ccb6b654fe985edd903b644aab75690
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-09-03 22:27:42 +0900

    쿼리 메서드와 Pageable의 결합

--- ideaProjects/ex02/src/test/java/org/zerock/ex02/repository/MemoRepositoryTest.java
+++ ideaProjects/ex02/src/test/java/org/zerock/ex02/repository/MemoRepositoryTest.java
@@ -1,11 +1,18 @@
 package org.zerock.ex02.repository;
 
+import jakarta.transaction.Transactional;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.ex02.entity.Memo;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -26,4 +33,86 @@ public class MemoRepositoryTest {
             memoRepository.save(memo);
         });
     }
+
+
+    @Test
+    public void testSelect() {
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("==============================");
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    @Transactional
+    public void testSelect2() {
+        Long mno = 100L;
+
+        Memo memo = memoRepository.getOne(mno);
+
+        System.out.println("==============================");
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testUpdate() {
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+        memoRepository.save(memo);
+    }
+
+    @Test
+    public void testDelete() {
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0,10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println("fsddas");
+        System.out.println(result);
+        System.out.println("------------------------------------------");
+        System.out.println("Total Pages : " + result.getTotalPages());
+        System.out.println("Total Count : " + result.getTotalElements());
+        System.out.println("Page Number : " + result.getNumber());
+        System.out.println("Page Size : " + result.getSize());
+        System.out.println("has next Page? : " + result.hasNext());
+        System.out.println("first page? : " + result.isFirst());
+        System.out.println("result.getContent() : " + result.getContent());
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testPage() {
+
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);
+
+        Pageable pageable = PageRequest.of(0,10,sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        // get() -> Stream
+        // getContent() -> List<?>
+        result.get().forEach(System.out::println);
+    }
+
+    @Test
+    public void testQueryMethod() {
+        List<Memo> result = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+        result.forEach(System.out::println);
+    }
+
+
 }
