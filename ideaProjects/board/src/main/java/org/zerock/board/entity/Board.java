20023613021d58e68270fb5455d8200351b11739
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-09-20 23:20:22 +0900

    @ManyToOne 추가

---
+++ ideaProjects/board/src/main/java/org/zerock/board/entity/Board.java
@@ -0,0 +1,24 @@
+package org.zerock.board.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "writer") //@ToString 은 항상 exclude
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String Content;
+
+    @ManyToOne
+    private Member writer;
+}
---
+++ ideaProjects/board/src/test/java/org/zerock/repository/ReplyRepositoryTests.java
@@ -0,0 +1,4 @@
+package org.zerock.repository;
+
+public class ReplyRepositoryTests {
+}
