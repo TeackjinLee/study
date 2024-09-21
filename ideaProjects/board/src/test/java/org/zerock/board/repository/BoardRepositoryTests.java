f346e5aac2b102c0972877df762299f9b132a249
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-09-21 19:15:23 +0900

    JPQL과 left (outer) join2

--- ideaProjects/board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ ideaProjects/board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -4,6 +4,10 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 import org.zerock.board.entity.Reply;
@@ -81,4 +85,28 @@ public class BoardRepositoryTests {
         }
 
     }
+
+    @Test
+    public void testWithReplyCount() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
+        });
+
+    }
+
+    @Test
+    public void testRead3() {
+        Object result = boardRepository.getBoardByBno(100L);
+
+        Object[] arr = (Object[]) result;
+
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
