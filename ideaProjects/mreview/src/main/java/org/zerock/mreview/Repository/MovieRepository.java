e47e9e1b8669ff0065c5f24bf96ad2adc2bb6fd3
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-10-03 18:44:49 +0900

    필요한 데이터 처리

--- ideaProjects/mreview/src/main/java/org/zerock/mreview/Repository/MovieRepository.java
+++ ideaProjects/mreview/src/main/java/org/zerock/mreview/Repository/MovieRepository.java
@@ -1,8 +1,12 @@
 package org.zerock.mreview.Repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.mreview.entity.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
+    @Query("SELECT m, mi, avg(coalesce(r.grade, 0)), count(distinct r) FROM Movie m LEFT JOIN MovieImage mi ON mi.movie = m LEFT JOIN Review r ON r.movie = m GROUP BY m")
+    Page<Object[]> getListPage(Pageable pageable);
 }
