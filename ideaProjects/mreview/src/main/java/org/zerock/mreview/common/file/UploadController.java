ca8b604635fb641f6ae894e0eec0efa5669e733a
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-10-06 14:45:40 +0900

    업로드 이미지 출력하기

--- ideaProjects/mreview/src/main/java/org/zerock/mreview/common/file/UploadController.java
+++ ideaProjects/mreview/src/main/java/org/zerock/mreview/common/file/UploadController.java
@@ -3,16 +3,26 @@ package org.zerock.mreview.common.file;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.common.file.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,7 +34,9 @@ public class UploadController {
 
 
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) {
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+
+        List<UploadResultDTO> uploadResultDTOList = new ArrayList<>();
 
         /******파일 저장하는 단계 고려사항*******
          * 업로드된 확장자가 이미지만 가능하도록 검사(첨부파일을 이용한 원격 쉘)
@@ -36,7 +48,7 @@ public class UploadController {
             // 이미지 파일만 업로드 가능
             if (uploadFile.getContentType().startsWith("image") == false) {
                 log.warn("this file is not image tpye");
-                return;
+                return new ResponseEntity<>((HttpStatus.FORBIDDEN));
             }
 
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
@@ -56,11 +68,13 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try {
-                uploadFile.transferTo(savePath);
+                uploadFile.transferTo(savePath); // 실제 이미지 저장
+                uploadResultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        } // end for
+        return new ResponseEntity<>(uploadResultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder() {
@@ -74,5 +88,34 @@ public class UploadController {
             uploadPathFolder.mkdirs();
         }
         return folderPath;
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("srcFileName: " + srcFileName);
+            System.out.println("fdsfsd : " + File.separator);
+            System.out.println(File.separator);
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
     }
 }
