de0b179fb0154d73a72749ae3cd1c7c0159eb7ed
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-10-19 00:22:25 +0900

    read.html에서의 영화리뷰 수정과삭제 작업 완료

--- ideaProjects/mreview/src/main/java/org/zerock/mreview/review/service/ReviewServiceImpl.java
+++ ideaProjects/mreview/src/main/java/org/zerock/mreview/review/service/ReviewServiceImpl.java
@@ -1,5 +1,6 @@
 package org.zerock.mreview.review.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -42,11 +43,12 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
+    @Transactional
     public void modify(ReviewDTO movieReviewDTO) {
         Optional<Review> result = reviewRepository.findById(movieReviewDTO.getReviewnum());
-
+        System.out.println("fff::::");
         if(result.isPresent()) {
-
+            System.out.println("fff::::::::");
             Review movieReview = result.get();
             movieReview.changeGrade(movieReviewDTO.getGrade());
             movieReview.changeText(movieReviewDTO.getText());
