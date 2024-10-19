f8bf86c9ed84680922c99181b1281ee273694fba
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-10-19 14:35:27 +0900

    Spring Boot와 Spring Security 용어의 흐름

---
+++ ideaProjects/club/src/main/java/org/zerock/club/controller/SampleController.java
@@ -0,0 +1,29 @@
+package org.zerock.club.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/sample")
+public class SampleController {
+    /* 로그인 하지 않은 사용자도 접근할 수 있는 페이지 */
+    @GetMapping("/all")
+    public void exAll() {
+        log.info("exAll......");
+    }
+
+    /* 로그인한 사용자만 접근할 수 있는 페이지 */
+    @GetMapping("/member")
+    public void exMember() {
+        log.info("exMember.......");
+    }
+
+    /* 관리자 권한이 있는 사용자만 접근할 수 있는 페이지 */
+    @GetMapping("/admin")
+    public void exAdmin() {
+        log.info("exAdmin......");
+    }
+}
