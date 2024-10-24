3bd1e6e11ce0a9e0364afddd64cd387ed90b8adc
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-10-24 23:11:23 +0900

    Spring Boot와 Spring Security Remember me와 @PreAuthorize

--- ideaProjects/club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ ideaProjects/club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,35 +1,28 @@
 package org.zerock.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.zerock.club.security.handler.ClubLoginSuccessHandler;
-
-import java.util.HashSet;
-import java.util.Set;
+import org.zerock.club.security.service.ClubUserDetailsService;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @Log4j2
 public class SecurityConfig {
+
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -51,18 +44,26 @@ public class SecurityConfig {
 
         log.info("----------------------------filterChain-------------------------------");
 
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**", "/login/**", "/sample/all").permitAll()
-                    .requestMatchers("/sample/member").hasAnyAuthority("OAUTH2_USER", "ROLE_USER")
-                        .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults()) // 기본 form 로그인 사용
-                .logout(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        /*http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**", "/login/**", "/sample/all").permitAll()
+                .requestMatchers("/sample/member").hasAnyAuthority("OAUTH2_USER", "ROLE_USER")
+                .anyRequest().authenticated()
+        );*/
+
+        http.formLogin(withDefaults()); // 기본 form 로그인 사용
+        http.logout(withDefaults());
 
         http.oauth2Login(oauth2Login ->
                     oauth2Login.successHandler(clubLoginSuccessHandler())
                 );
+
+        // 자동로그인 기능
+        http.rememberMe(remember -> remember
+                .tokenValiditySeconds(60 * 60 * 24 * 7) // 7일지정
+                .userDetailsService(userDetailsService)
+        );
 
         return http.build();
     };
--- ideaProjects/club/src/main/java/org/zerock/club/controller/SampleController.java
+++ ideaProjects/club/src/main/java/org/zerock/club/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.club.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.zerock.club.security.dto.ClubAuthMemberDTO;
 @RequestMapping("/sample")
 public class SampleController {
     /* 로그인 하지 않은 사용자도 접근할 수 있는 페이지 */
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll() {
         log.info("exAll......");
@@ -27,8 +29,18 @@ public class SampleController {
     }
 
     /* 관리자 권한이 있는 사용자만 접근할 수 있는 페이지 */
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin() {
         log.info("exAdmin......");
+    }
+
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@zerock.com\"")   // 특정 사용자만 허용
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember) {
+        log.info("exMemberOnly......");
+        log.info(clubAuthMember);
+
+        return "/sample/admin";
     }
 }
